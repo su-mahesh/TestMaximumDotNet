caf8876ed5bc822c28d724fd13bba7900292b3a0
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-12 21:02:29 +0530

    [MaheshKangude] ADDED: GetMaximumIntegerValue WhenAtThirdPosition

--- NUnitTestProject/UnitTest.cs
+++ NUnitTestProject/UnitTest.cs
@@ -12,17 +12,25 @@ namespace NUnitTestProject
         {
             testMaximum = new TestMaximum();
         }
-
+        //FIND MAXIMUM INTEGER
         [Test]
         public void GivenMaxInteger_AtFirstPosition_ShouldReturnTheSameNumber()
         {
             int result = testMaximum.GetMaximumInteger(76, 50, 34);
             Assert.AreEqual(76, result);
         }
+
         [Test]
         public void GivenMaxInteger_AtSecondPosition_ShouldReturnTheSameNumber()
         {
             int result = testMaximum.GetMaximumInteger(50, 76, 34);
+            Assert.AreEqual(76, result);
+        }
+
+        [Test]
+        public void GivenMaxInteger_AtThirdPosition_ShouldReturnTheSameNumber()
+        {
+            int result = testMaximum.GetMaximumInteger(50, 34, 76);
             Assert.AreEqual(76, result);
         }
     }
